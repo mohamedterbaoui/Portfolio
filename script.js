b350452c64ef130c567515dd00a0dcd9920f4e53
Mohamed Terbaoui <m.terbaoui7@gmail.com>
2025-02-01 10:48:22 -0500

    Implement navigation in menu

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // Getting all list items
 const menuItems = document.querySelectorAll("#menu li");
+const navSections = document.querySelectorAll(".section")
+
+// Function to activate a section
+function activateSection(index) {
+    // Remove 'active from all sections and menu items
+    menuItems.forEach(i => i.classList.remove('active'));
+    navSections.forEach(section => section.classList.remove('active_section'));
+
+    // Add 'active' class to the clicked menu item and corresponding section
+    menuItems[index].classList.add('active');
+    navSections[index].classList.add('active_section');
+}
+
+// Set the first menu item and section as active by default
+document.addEventListener("DOMContentLoaded", () =>{
+    activateSection(0);
+});
 
 // Handling the click event on the navigation
-menuItems.forEach(item => {
+menuItems.forEach((item, index) => {
     item.addEventListener('click', () => {
-        menuItems.forEach(i => i.classList.remove('active'));
-
-        item.classList.add('active');
+        activateSection(index);
     })
 })
 
